d459a1e11852dd99a35435ad873742055f7f7a78
Lorenzo Godi <lorenzo.godi@outlook.it>
2017-12-28 22:50:24 +0100

    [Astronomy picture of the day] è completata al 100%

--- NasaExplorer/Pagine Secondarie/SpecApodPage.xaml.cs
+++ NasaExplorer/Pagine Secondarie/SpecApodPage.xaml.cs
@@ -13,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // Il modello di elemento Pagina vuota è documentato all'indirizzo https://go.microsoft.com/fwlink/?LinkId=234238
@@ -35,28 +36,65 @@ namespace NasaExplorer
             collection.Add(theme);
             this.Transitions = collection;
 
+            //
 
             this.InitializeComponent();
+
+            //
+
+            Download();
+        }
+
+        private async void Download()
+        {
+            await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                grid_caricamento.Visibility = Visibility.Visible;
+                //scrollvw.Visibility = Visibility.Collapsed;
+            });
+
+            //
+
+            string date = $"&date={mydate.Date.Year}-{mydate.Date.Month}-{mydate.Date.Day}";
+            string url = "https://api.nasa.gov/planetary/apod?api_key=eyzZKG6yo1by0opmheMRLsOtUZnp0yUpKSSI5vzD" + date;
+
+            //
+
+            try
+            {
+                var winrtClient = new Windows.Web.Http.HttpClient();
+                string k = await winrtClient.GetStringAsync(new Uri(url));
+
+                JObject cose = JObject.Parse(k);
+                string url_img = cose["url"].ToString();
+
+                //
+
+                Uri uri = new Uri(BaseUri, url_img);
+                BitmapImage imgSource = new BitmapImage(uri);
+
+                await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                {
+                    myimage.Source = imgSource;
+                    descrizione.Text = cose["explanation"].ToString();
+                });
+            }
+            catch
+            {
+
+            }
+            
         }
 
         private void DatePicker_DateChanged(object sender, DatePickerValueChangedEventArgs e)
         {
-            string date = $"&date={mydate.Date.Year}-{mydate.Date.Month}-{mydate.Date.Day}";
-            string url = "https://api.nasa.gov/planetary/apod?api_key=eyzZKG6yo1by0opmheMRLsOtUZnp0yUpKSSI5vzD" + date;
-            CaricaImmagine_apod(url);
+            Download();
         }
 
-        public async void CaricaImmagine_apod(string url)
+        private void myimage_ImageOpened(object sender, RoutedEventArgs e)
         {
-            var winrtClient = new Windows.Web.Http.HttpClient();
-            string k = await winrtClient.GetStringAsync(new Uri(url));
-
-            JObject cose = JObject.Parse(k);
-
-            await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
-            {
-                myimage.Source = new Windows.UI.Xaml.Media.Imaging.BitmapImage(new Uri(cose["url"].ToString(), UriKind.Absolute));
-            });
+            scrollvw.Visibility = Visibility.Visible;
+            grid_caricamento.Visibility = Visibility.Collapsed;
         }
     }
 }
