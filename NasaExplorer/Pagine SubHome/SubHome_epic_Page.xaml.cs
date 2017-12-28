388e59557fa72d6624afc15d708057850d6dd4fa
Lorenzo Godi <lorenzo.godi@outlook.it>
2017-12-28 18:26:25 +0100

    Riorganizzazione completata, mancano funzionalità aggiuntive Il codice dell'App è stato RISCRITTO DA CAPO.

--- NasaExplorer/Pagine SubHome/SubHome_epic_Page.xaml.cs
+++ NasaExplorer/Pagine SubHome/SubHome_epic_Page.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -11,6 +12,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // Il modello di elemento Pagina vuota è documentato all'indirizzo https://go.microsoft.com/fwlink/?LinkId=234238
@@ -25,6 +27,40 @@ namespace NasaExplorer
         public SubHome_epic_Page()
         {
             this.InitializeComponent();
+            //JJJ();
+            Download("https://epic.gsfc.nasa.gov/epic-archive/jpg/epic_1b_20151031003633.jpg");
+        }
+
+        private async void JJJ()
+        {
+            const string starting_url = "";
+            var winrtClient = new Windows.Web.Http.HttpClient();
+            string k = await winrtClient.GetStringAsync(new Uri(starting_url));
+            JObject cose = JObject.Parse(k);
+
+            //
+
+            string url_img = cose["url"].ToString();
+
+            //
+
+            Download(url_img);
+        }
+
+        private async void Download(string dwn_url)
+        {
+            Uri uri = new Uri(BaseUri, dwn_url);
+            BitmapImage imgSource = new BitmapImage(uri);
+
+            await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                Img.Source = imgSource;
+            });
+        }
+
+        private void Img_ImageOpened(object sender, RoutedEventArgs e)
+        {
+            grid_caricamento.Visibility = Visibility.Collapsed;
         }
     }
 }
--- NasaExplorer/Pagine SubHome/SubHome_rover_Page.xaml.cs
+++ NasaExplorer/Pagine SubHome/SubHome_rover_Page.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NasaExplorer.Classi;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -11,6 +13,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // Il modello di elemento Pagina vuota è documentato all'indirizzo https://go.microsoft.com/fwlink/?LinkId=234238
@@ -25,6 +28,51 @@ namespace NasaExplorer
         public SubHome_rover_Page()
         {
             this.InitializeComponent();
+            JJJ();
+        }
+
+        private async void JJJ()
+        {
+            try
+            {
+                Int32.Parse(LocalStorageUtility.RitornaStringa("ultimoconosciuto"));
+            }
+            catch
+            {
+                LocalStorageUtility.SetStringa("1909", "ultimoconosciuto");
+            }
+
+            //
+
+            int ultimoconosciuto = Int32.Parse(LocalStorageUtility.RitornaStringa("ultimoconosciuto"));
+            string starting_url = $"https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key={ApiUtility.APIKEY}&sol={ultimoconosciuto}";
+            var winrtClient = new Windows.Web.Http.HttpClient();
+            string k = await winrtClient.GetStringAsync(new Uri(starting_url));
+
+            //
+
+            JMars1 jm = JMars1.FromJson(k);
+            string url_img = jm.Photos[0].ImgSrc;
+
+            //
+
+            Download(url_img);
+        }
+
+        private async void Download(string dwn_url)
+        {
+            Uri uri = new Uri(BaseUri, dwn_url);
+            BitmapImage imgSource = new BitmapImage(uri);
+
+            await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                Img.Source = imgSource;
+            });
+        }
+
+        private void Img_ImageOpened(object sender, RoutedEventArgs e)
+        {
+            grid_caricamento.Visibility = Visibility.Collapsed;
         }
     }
 }
